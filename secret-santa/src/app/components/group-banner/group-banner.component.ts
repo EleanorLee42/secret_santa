a7c41c4ca47be5f294e82377febf66267c632020
Eleanor Lee <el.42.train@gmail.com>
2022-12-09 15:26:32 -0500

    edit profile rough draft

--- secret-santa/src/app/components/group-banner/group-banner.component.ts
+++ secret-santa/src/app/components/group-banner/group-banner.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'group-banner',
@@ -8,15 +7,7 @@ import { Router } from '@angular/router';
 })
 export class GroupBannerComponent implements OnInit {
 
-  constructor( private router: Router ) { }
-
-  /* goToGroupView is called when a group-banner is clicked.
-     It uses the injected router to navigate to the group-view page.
-  */
-  goToGroupView() {
-    // navigation with a button from https://www.folkstalk.com/2022/09/button-click-navigate-to-another-page-angular-with-code-examples-2.html
-    this.router.navigateByUrl('/group-view');
-  }
+  constructor() { }
 
   ngOnInit() {}
 
