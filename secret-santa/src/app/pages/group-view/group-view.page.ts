dd5768903654c0083ad2a2645cde05955875cd79
Eleanor Lee <el.42.train@gmail.com>
2022-12-09 13:14:13 -0500

    group-view pg rough draft

--- secret-santa/src/app/pages/group-view/group-view.page.ts
+++ secret-santa/src/app/pages/group-view/group-view.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-group-view',
@@ -7,7 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GroupViewPage implements OnInit {
 
-  constructor() { }
+  constructor( private router: Router ) { }
+
+  /* eventOnClick() is called when a person is clicked.
+     It navigates to the person-view page. 
+  */
+  eventOnClick() {
+    this.router.navigateByUrl('/person-view');
+  }
 
   ngOnInit() {
   }
