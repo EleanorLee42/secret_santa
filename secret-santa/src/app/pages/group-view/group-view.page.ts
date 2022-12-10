074a710cc1c910cdaa5c9687d4ba3b2f5cb97175
vgvreug <90860265+vgvreug@users.noreply.github.com>
2022-12-10 16:48:12 -0500

    notification fixes

--- secret-santa/src/app/pages/group-view/group-view.page.ts
+++ secret-santa/src/app/pages/group-view/group-view.page.ts
@@ -3,10 +3,11 @@ import { AlertController } from '@ionic/angular';
 import { getMessaging } from "firebase/messaging";
 import { initializeApp } from "firebase/app";
 import { environment } from '../../../environments/environment';
-import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+import { AngularFirestore, AngularFirestoreCollection, fromCollectionRef } from '@angular/fire/compat/firestore';
 import { AngularFireMessaging } from '@angular/fire/compat/messaging';
 import { ActivatedRoute, Router } from '@angular/router';
-import { getDefaultCompilerOptions } from 'typescript';
+import { getDefaultCompilerOptions, getDefaultFormatCodeSettings } from 'typescript';
+import { getDocs, collection } from 'firebase/firestore'
 
 interface MiniPerson {
   Name: string,
@@ -45,9 +46,9 @@ interface Person {
 export class GroupViewPage implements OnInit {
   groupID!: string;  //Id of current group
   private peopleCollection: AngularFirestoreCollection<Person> = this.db.collection<Person>('/People');
-  people: Person[] | undefined;
+  people: Person[];
   // private groupCollection: AngularFirestoreCollection<Group> = this.db.collection<Group>('/Groups');
-  group: Group | undefined; // Current group
+  group: Group; // Current group
   messaging = getMessaging(initializeApp(environment.firebase));
 
   constructor(private alertCtrl: AlertController,
@@ -57,75 +58,85 @@ export class GroupViewPage implements OnInit {
     this.listenForMessages();
   }
 
-  getgroupData = async () => {
-    let groupDoc = await this.db.collection<Group>('/Groups').ref.doc(this.groupID).get();
-    return {
-      Name: groupDoc.get("Name"),
-      joinCode: groupDoc.get("joinCode"),
-      numPeople: groupDoc.get("numPeople"),
-      miniPeople: groupDoc.get("people"),
-      id: groupDoc.id
-    }
-  }
-  getIds = async (group: Group): Promise<string[]> => {
-    console.log(group.miniPeople);
-    let ids: string[] = [];
-    group.miniPeople.forEach(element => {
-      ids.push(element.id);
-    });
-    console.log(ids);
-    return ids;
-  }
+  // getgroupData = async () => {
+  //   let groupDoc = await this.db.collection<Group>('/Groups').ref.doc(this.groupID).get();
+  //   return {
+  //     Name: groupDoc.get("Name"),
+  //     joinCode: groupDoc.get("joinCode"),
+  //     numPeople: groupDoc.get("numPeople"),
+  //     miniPeople: groupDoc.get("People"),
+  //     id: groupDoc.id
+  //   }
+  // }
+  // getIds = async (group: Group): Promise<string[]> => {
+  //   console.log(group);
+  //   console.log(group.miniPeople);
+  //   let ids: string[] = [];
+  //   group.miniPeople.forEach(element => {
+  //     ids.push(element.id);
+  //   });
+  //   console.log(ids);
+  //   return ids;
+  // }
 
   async ngOnInit() {
     this.groupID = String(this.route.snapshot.paramMap.get('id')); //gets groupID from route parameter
-    console.log(this.groupID);
+    // console.log(this.groupID);
     // let groupDoc = await this.db.collection<Group>('/Groups').ref.doc(this.groupID).get();
-    let group1 = await this.getgroupData();
-
-
-    const ids: string[] = await this.getIds(group1);
+    // let group1 = await this.getgroupData();
+    let groupDoc = await this.db.collection<Group>('/Groups').ref.doc(this.groupID).get();
+    this.group = {
+      Name: groupDoc.get("Name"),
+      joinCode: groupDoc.get("joinCode"),
+      numPeople: groupDoc.get("numPeople"),
+      miniPeople: groupDoc.get("People"),
+      id: groupDoc.id
+    }
+    // const ids: string[] = await this.getIds(this.group);
+    let ids: string[] = [];
     // console.log(this.group.miniPeople);
-    // this.group.miniPeople.forEach(element => {
-    //   ids.push(element.id);
-    // });
-    console.log(ids);
-
-    let peopleSub = this.peopleCollection.snapshotChanges().subscribe(result => {
-      if (result) {
-        this.people = result.map((doc) => {
-          return {
-            Groups: doc.payload.doc.data().Groups,
-            Interests: doc.payload.doc.data().Interests,
-            Name: doc.payload.doc.data().Name,
-            PhoneNumber: doc.payload.doc.data().PhoneNumber,
-            Token: doc.payload.doc.data().Token,
-            email: doc.payload.doc.data().email,
-            id: doc.payload.doc.id
-          };
-        })
-      }
+    this.group.miniPeople.forEach(element => {
+      ids.push(element.id);
     });
+    // console.log(ids);
+
+    let peopleSnap = await this.db.collection("/People").ref.get();
+    this.people = peopleSnap.docs.map((doc) => {
+      return {
+        Groups: doc.get("Groups"),
+        Interests: doc.get("Interests"),
+        Name: doc.get("Name"),
+        PhoneNumber: doc.get("PhoneNumber"),
+        Token: doc.get("Token"),
+        email: doc.get("email"),
+        id: doc.id
+      };
+    })
     //list of people in currect group
-    this.people = this.people?.filter(person => ids.includes(person.id));
+    this.people = this.people.filter(person => ids.includes(person.id));
   }
 
   assignPartners = () => {
+    console.log(this.people);
     //Fisher-Yates shuffle from w3schools
-    let groupPeople = this.group?.miniPeople;
-    for (let i = groupPeople!.length - 1; i > 0; i--) {
+    let groupPeople = this.group.miniPeople;
+    for (let i = groupPeople.length - 1; i > 0; i--) {
       let j = Math.floor(Math.random() * (i + 1));
       let k = groupPeople![i];
-      groupPeople![i] = groupPeople![j];
-      groupPeople![j] = k;
+      groupPeople[i] = groupPeople[j];
+      groupPeople[j] = k;
     }
     let peopleIndex: number;
     let groupIndex: number;
     for (let i = 0; i < groupPeople!.length; i++) {
-      peopleIndex = this.people!.findIndex((person: Person) => person.id === groupPeople![i].id)
-      groupIndex = this.people![peopleIndex].Groups.findIndex((group: MiniGroup) => group.GroupID === this.group!.id)
-      this.people![peopleIndex].Groups[groupIndex].GifteeID = groupPeople![(i + 1) % groupPeople!.length].id;
-      this.people![peopleIndex].Groups[groupIndex].GifteeName = groupPeople![(i + 1) % groupPeople!.length].Name;
+      peopleIndex = this.people.findIndex((person: Person) => person.id === groupPeople[i].id)
+      console.log(this.group.id);
+      groupIndex = this.people[peopleIndex].Groups.findIndex((group: MiniGroup) => group.GroupID === this.group.id)
+      console.log("i is: " + i + "and the thing is: ");
+      console.log(groupIndex);
+      console.log(this.people[peopleIndex].Groups);
+      this.people[peopleIndex].Groups[groupIndex].GifteeID = groupPeople[(i + 1) % groupPeople.length].id;
+      this.people[peopleIndex].Groups[groupIndex].GifteeName = groupPeople[(i + 1) % groupPeople.length].Name;
       this.peopleCollection.doc(this.people![peopleIndex].id).update(this.people![peopleIndex]);
     }
     console.log(this.people);
